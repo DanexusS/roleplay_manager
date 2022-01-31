b840ba99f1c4503f401d6e0e81e279b9fd3cdd89
DanexusS <mrcherese.lite@yandex.ru>
2022-01-31 20:27:28 +0300

    Идёт изучение апи дискорда, добавлена пара команд

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from discord_slash.utils.manage_commands import create_option
 TOKEN = "NTY3MzMyNTU5NDc5MTExNzQw.XLR_ng.zhaxoAo_6ZL-LfA5gBEZXPAfGj0"
 PREFIX = "/"
 server_id = 936293335063232672
-client = commands.Bot(command_prefix=PREFIX)
+activity = discord.Activity(type=discord.ActivityType.listening, name="шутки про хохлов")
+client = commands.Bot(command_prefix=PREFIX, activity=activity)
 slash = SlashCommand(client, sync_commands=True)
 
 
@@ -24,5 +25,15 @@ async def on_ready():
 )
 async def hi(ctx):
     await ctx.send("Hello")
+
+
+@slash.slash(
+    name="hi_member",
+    description="says hi1",
+    options=[{"name": "member", "description": "пользователь", "type": 6, "required": True}],
+    guild_ids=[server_id]
+)
+async def hi_member(ctx, member: discord.Member = None):
+    await ctx.send(f"Hello {member.mention}")
 
 client.run(TOKEN)
